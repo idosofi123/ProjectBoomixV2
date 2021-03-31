acae24f5ac4d72e439d298f980e99fb2404b30f7
Ido Michael Sofi <pikagms@gmail.com>
2021-03-31 20:20:46 +0300

    starting to work on drawing system

--- ProjectBoomixV2/ProjectBoomixClient/Screening/Screens/InGameScreen.cs
+++ ProjectBoomixV2/ProjectBoomixClient/Screening/Screens/InGameScreen.cs
@@ -11,6 +11,8 @@ namespace ProjectBoomixClient.Screening.Screens {
 
     public sealed class InGameScreen : Screen {
 
+        
+
         public override void Init(ContentManager contentManager) {
 
         }
@@ -39,6 +41,7 @@ namespace ProjectBoomixClient.Screening.Screens {
             spriteBatch.Begin(SpriteSortMode.Deferred, null, null, null, null, null, VirtualResolution.GetVirtualResolutionScale());
             spriteBatch.DrawString(GlobalResources.RegularFont, "In Game", new Vector2(40, 30), Color.White);
             spriteBatch.DrawString(GlobalResources.RegularFont, $"FPS: {(int)Math.Round(1 / gameTime.ElapsedGameTime.TotalSeconds)}", new Vector2(1740, 30), Color.White);
+
             spriteBatch.End();
         }
 
